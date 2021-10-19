1cdb1dba9a7351d9da6991a1d4e81f41e74673cf
Rosa Baker <rpintopa@gmail.com>
2021-10-19 17:43:28 -0600

    Characters fixed

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -2,18 +2,15 @@ import { films } from '../data/films.js'
 
 let filmList = document.querySelector('#filmList')
 
-
-
-let figure = document.createElement('figure')
-let figImg = document.createElement('img')
-figImg.src =  `https://starwars-visualguide.com/assets/img/films/1.jpg`
-let figCaption = document.createElement('figcaption')
-
-
-filmList.appendChild(poster)
-
 for (let i = 0; i < films.length; i++) {
-    let titleItem = document.createElement('li')
-    titleItem.textContent = films[i].title
-    titleList.appendChild(titleItem)
-}
+  let figure = document.createElement('figure')
+  let figImg = document.createElement('img')
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+  let figCaption = document.createElement('figcaption')
+  figCaption.textContent = films[i].title
+  
+  figure.appendChild(figImg)
+  figure.appendChild(figCaption)
+  
+  filmList.appendChild(figure)
+ }
