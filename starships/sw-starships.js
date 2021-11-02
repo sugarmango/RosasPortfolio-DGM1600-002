56e5f2ee23eb1965b789d7e56201017dda6ddecf
Rosa Baker <rpintopa@gmail.com>
2021-11-02 12:41:09 -0600

    Mostly done with class code

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,31 +1,48 @@
-import { starships } from "../data/starships.js";
-import { getLastNumber, removeChildren } from "../utils/index.js";
+import { starships } from '../data/starships.js';
+import { getLastNumber, removeChildren } from '../utils/index.js';
 
-const nav = document.querySelector('.nav')
-const navList = document.querySelector('.navList')
-const shipView = document.querySelector('.shipViewer')
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.navList');
+const shipView = document.querySelector('.shipViewer');
+
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.modal-close');
+const modalBackground = document.querySelector('.modal-background');
+const shipMessage
+
+closeButton.addEventListener('click', () => modal.classList.toggle('is-active'));
+modalBackground.addEventListener('click', () => modal.classList.toggle('is-active'));
 
 function populateNav() {
-starships.forEach(starship => {
-    let anchorWrap = document.createElement('a')
-    anchorWrap.href = '#'
-    let listItem = document.createElement('li')
-    listItem.textContent = starship.name
+	starships.forEach((starship) => {
+		let anchorWrap = document.createElement('a');
+		anchorWrap.href = '#';
+		let listItem = document.createElement('li');
+		listItem.textContent = starship.name;
 
-anchorWrap.addEventListener('click', () => populateShipView(starship) )
+		anchorWrap.addEventListener('click', () => populateShipView(starship));
 
-    anchorWrap.appendChild(listItem)
-    navList.appendChild(anchorWrap)
-})
+		anchorWrap.appendChild(listItem);
+		navList.appendChild(anchorWrap);
+	});
 }
 
-populateNav()
+populateNav();
 
 function populateShipView(shipData) {
-    removeChildren(shipView)
-console.log(`You clicked on ${shipData.name}`)
-let shipImage = document.createElement('img')
-let shipNum = getLastNumber(shipData.url)
-shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-shipView.appendChild(shipImage)
+	removeChildren(shipView);
+	console.log(`You clicked on ${shipData.name}`);
+	let shipImage = document.createElement('img');
+    let shipName = document.createElement('div')
+    shipName.className = 'shipName'
+    shipName.textContent = shipData.name
+	let shipNum = getLastNumber(shipData.url);
+	shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+	shipImage.addEventListener('error', () => {
+		shipImage.hidden = true;
+        shipName.hidden = true;
+		modal.classList.toggle('is-active');
+        shipMessage.textContent = `The ship known as "${shipData.name}" has been canceled.`
+	});
+	shipView.appendChild(shipImage);
 }
