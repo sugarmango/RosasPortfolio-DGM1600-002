bd13e9b08988897e47b4ca1103b393b9907a9d4d
Rosa Baker <rpintopa@gmail.com>
2021-11-20 22:15:34 -0700

    Added pointers to main and congress

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,22 +1,22 @@
 async function getAPIData(url) {
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		return data
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`)
-.then((data) => {
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`)
+  .then((data) => {
     console.log(data)
     populatePokeCards(data)
-})
-
-const pokeGrid = document.querySelector('.pokeGrid')
-
-function populatePokeCards(singlePokemon) {
+  })
+  
+  const pokeGrid = document.querySelector('.pokeGrid')
+  
+  function populatePokeCards(singlePokemon) {
     const pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     const pokeCard = document.createElement('div')
@@ -26,11 +26,10 @@ function populatePokeCards(singlePokemon) {
     pokeFront.textContent = 'Front'
     const pokeBack = document.createElement('div')
     pokeBack.className = 'cardFace back'
-    pokeBack.textContent = 'singlePokemon.name'
-
+    pokeBack.textContent = singlePokemon.name
+  
     pokeCard.appendChild(pokeFront)
     pokeCard.appendChild(pokeBack)
     pokeScene.appendChild(pokeCard)
     pokeGrid.appendChild(pokeScene)
-
-}
+  }
