6463d1c8b5f198d0f4d14f9576d23eff4edd6149
Rosa Baker <rpintopa@gmail.com>
2021-11-27 21:55:02 -0700

    Added filters and button styling to congress

--- congress/congress.js
+++ congress/congress.js
@@ -1,5 +1,6 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const members = [...senators, ...representatives] // modern combining arrays
 
@@ -8,33 +9,50 @@ const seniorityHeading = document.querySelector('.seniority')
 const weaselOrderedList = document.querySelector('.weaselList')
 
 function simplifiedMembers(chamberFilter) {
-  const filteredArray = members.filter(member => chamberFilter ? member.short_title === chamberFilter : member)
-  
-  return filteredArray.map(senator => {
-    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-    return {
-      id: senator.id,
-      name: `${senator.first_name}${middleName}${senator.last_name}`,
-      party: senator.party,
-      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
-      gender: senator.gender,
-      seniority: +senator.seniority,
-      missedVotesPct: senator.missed_votes_pct,
-      loyaltyPct: senator.votes_with_party_pct,
-    }
-  })
+  if(chamberFilter === "D" | chamberFilter === "R"){
+    const filteredArray = members.filter(member => chamberFilter ? member.party === chamberFilter : member)
+    return filteredArray.map(senator => {
+      const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+      return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        party: senator.party,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+        gender: senator.gender,
+        seniority: +senator.seniority,
+        missedVotesPct: senator.missed_votes_pct,
+        loyaltyPct: senator.votes_with_party_pct,
+      }
+    })
+  }
+  else{
+    const filteredArray = members.filter(member => chamberFilter ? member.short_title === chamberFilter : member)
+    return filteredArray.map(senator => {
+      const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+      return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        party: senator.party,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+        gender: senator.gender,
+        seniority: +senator.seniority,
+        missedVotesPct: senator.missed_votes_pct,
+        loyaltyPct: senator.votes_with_party_pct,
+      }
+    })
+  }
 }
 
 populateSenatorDiv(simplifiedMembers())
 
 function populateSenatorDiv(simpleSenators) {
+
   simpleSenators.forEach(senator => {
     let senFigure = document.createElement('figure')
     let figImg = document.createElement('img')
     let figCaption = document.createElement('figcaption')
 
     figImg.src = senator.imgURL
-
     figCaption.textContent = senator.name
     senFigure.appendChild(figImg)
     senFigure.appendChild(figCaption)
@@ -73,4 +91,22 @@ biggestWeasels.forEach(weasel => {
   let listItem = document.createElement('li')
   listItem.textContent = weasel.name
   weaselOrderedList.appendChild(listItem)
+})
+
+// 'D', 'R', 'Rep.', 'Sen.'
+document.getElementById("R").addEventListener("click", function() {
+  removeChildren(document.getElementById("Senators"))
+  populateSenatorDiv(simplifiedMembers("R"))
+})
+document.getElementById("D").addEventListener("click", function() {
+  removeChildren(document.getElementById("Senators"))
+  populateSenatorDiv(simplifiedMembers("D"))
+})
+document.getElementById("Rep").addEventListener("click", function() {
+  removeChildren(document.getElementById("Senators"))
+  populateSenatorDiv(simplifiedMembers("Rep."))
+})
+document.getElementById("Sen").addEventListener("click", function() {
+  removeChildren(document.getElementById("Senators"))
+  populateSenatorDiv(simplifiedMembers("Sen."))
 })
