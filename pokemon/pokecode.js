2d9866386f5d72a2dbd20cba201d00c6d55780fd
Rosa Baker <rpintopa@gmail.com>
2021-12-04 22:26:29 -0700

    Basic styling in Pokemon

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,27 +1,86 @@
+import { removeChildren } from '../utils/index.js'
+
 function getAPIData(url) {
   try {
-    return fetch(url).then(data => data.json())
+    return fetch(url).then((data) => data.json())
   } catch (error) {
     console.error(error)
   }
 }
 
-getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=898`)
-.then( async (data) => {
-  for (const pokemon of data.results) {
-    await getAPIData(pokemon.url).then(pokeData => populatePokeCards(pokeData))
-  }
-})
+function loadPokemon(offset = 0, limit = 25) {
+  getAPIData(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
+  ).then(async (data) => {
+    for (const pokemon of data.results) {
+      await getAPIData(pokemon.url).then((pokeData) =>
+        populatePokeCard(pokeData),
+      )
+    }
+  })
+}
 
 const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
+loadButton.addEventListener('click', () => {
+  removeChildren(pokeGrid)
+  loadPokemon(100, 5)
+})
 
-function populatePokeCards(singlePokemon) {
+const moreButton = document.querySelector('.morePokemon')
+moreButton.addEventListener('click', () => {
+  let limit = prompt('How many more Pokemon should I load?')
+  let offset = prompt('At which Pokemon ID should I start loading?')
+  loadPokemon(offset, limit)
+})
+
+const newButton = document.querySelector('.newPokemon')
+newButton.addEventListener('click', () => {
+  let pokeName = prompt('What is the name of your new Pokemon?')
+  let pokeHeight = prompt("What is the Pokemon's height?")
+  let pokeWeight = prompt("What is the Pokemon's weight?")
+  let pokeAbilities = prompt(
+    'What are your Pokemon abilities? (use a comma separated list)',
+  )
+  let newPokemon = new Pokemon(
+    pokeName,
+    pokeHeight,
+    pokeWeight,
+    getAbilitiesArray(pokeAbilities),
+  )
+  populatePokeCard(newPokemon)
+})
+
+function getAbilitiesArray(commaString) {
+  let tempArray = commaString.split(',')
+  return tempArray.map((abilityName) => {
+    return {
+      ability: {
+        name: abilityName,
+      },
+    }
+  })
+}
+
+class Pokemon {
+  constructor(name, height, weight, abilities) {
+    ;(this.id = 100),
+      (this.name = name),
+      (this.height = height),
+      (this.weight = weight),
+      (this.abilities = abilities)
+  }
+}
+
+function populatePokeCard(singlePokemon) {
   const pokeScene = document.createElement('div')
   pokeScene.className = 'scene'
   const pokeCard = document.createElement('div')
   pokeCard.className = 'card'
-  pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
-  
+  pokeCard.addEventListener('click', () =>
+    pokeCard.classList.toggle('is-flipped'),
+  )
+
   const front = populateCardFront(singlePokemon)
   const back = populateCardBack(singlePokemon)
 
@@ -41,7 +100,54 @@ function populateCardFront(pokemon) {
   pokeCaption.textContent = pokemon.name
   pokeFront.appendChild(pokeImg)
   pokeFront.appendChild(pokeCaption)
+
+  //typesBackground(pokemon, pokeFront)
   return pokeFront
+}
+
+function typesBackground(pokemon, card) {
+  let pokeType1 = pokemon.types[0].type.name
+  let pokeType2 = pokemon.types[1]?.type.name
+  console.log(pokeType1, pokeType2)
+  card.style.setProperty(
+    'background',
+    `linear-gradient(${getPokeTypeColor(pokeType1)}, #FFF})`,
+  )
+}
+
+function getPokeTypeColor(pokeType) {
+  let color
+  switch (pokeType) {
+    case 'grass':
+      color = '#00FF00'
+      break
+    case 'fire':
+      color = '#FF0000'
+      break
+    case 'water':
+      color = '#0000FF'
+      break
+    case 'bug':
+      color = '#7FFF00'
+      break
+    case 'normal':
+      color = '#F5F5DC'
+      break
+    case 'flying':
+      color = '#00FFFF'
+      break
+    case 'poison':
+      color = '#C300FF'
+      break
+    case 'electric':
+      color = '#C8FF00'
+      break
+      case 'psychic':
+        color = '#333333'
+        break
+    default:
+      color = '#888888'
+  }
 }
 
 function populateCardBack(pokemon) {
