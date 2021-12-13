233fc8e77306107fb6f556e845ac18bf026c0aa5
Rosa Baker <rpintopa@gmail.com>
2021-12-12 17:09:27 -0700

    Added password and image to sp

--- secret/secret.js
+++ secret/secret.js
@@ -1,7 +1,7 @@
 
 function run(){
-    var password = prompt("Password Please");
-    if(password != 'greatestfriend'){
+    var password = prompt("Enter the Password Please");
+    if(password != 'gf'){
     document.body.innerHTML = '';
     document.body.innerHTML = 'Password Failed! Reload to Renter Password';
     }else{
